77a664ce5cf1b72b86dade18a83b703803bc72c3
chenqingyun <1209324073@qq.com>
2022-11-15 11:15:52 +0800

    米游社获取cookie

---
+++ mihoyoCookie.js
@@ -0,0 +1,19 @@
+const title = "米游社";
+const subTitleNew = "首次添加";
+const subTitleUpdate = "更新";
+!(async() => {
+    getCookie();
+});
+function getCookie(){
+    var cookie = $request.headers.Cookie;
+    var currentCookie = $prefs.valueForKey("mihoyoCookie");
+    if(currentCookie != undefined){
+        console.log("正在更新cookie。。。");
+        $prefs.setValueForKey(cookie,"mihoyoCookie");
+        $notify(title,subTitleUpdate,"更新cookie成功");
+    }else{
+        console.log("正在添加cookie。。。");
+        $prefs.setValueForKey(cookie,"mihoyoCookie");
+        $notify(title,subTitleNew,"添加cookie成功");
+    }
+}
